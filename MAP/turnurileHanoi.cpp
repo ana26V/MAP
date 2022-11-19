762683b75511103e9a8e33a9eddfff6c6c7e8c4e
ana26V <anamariavoichescu26@gmail.com>
2022-11-19 18:23:37 +0200

    reguli afisate mai dragut

--- MAP/turnurileHanoi.cpp
+++ MAP/turnurileHanoi.cpp
@@ -17,47 +17,57 @@ void turnurileHanoi(int n, char tija_init, char tija_finala,char tija_aux)
 void reguli()
 {
     system("color A");
-    cout << "\t\tTurnurile din Hanoi" << endl;
-    cout << "Se dau trei tije A, B, C si N discuri de diametre diferite, stivuite initial pe tija A\n"
-        " in ordinea descrescatoare a diametrelor, formand un con. Ss se determine secventa de\n"
-        "mutari ale discurilor de pe tija initială A catre tija finala C, folosind B ca tija ,\n"
-        " avand în vedere urmatoarele restrictii : \n"
-        "  a.la fiecare miscare se va muta un singur disc\n"
-        "  b. un disc se poate plasa doar asupra unui disc mai mare ca diametru." << endl;
-    _getch();  system("cls");
+    cout << "| ==================================================================================================|" << endl;
+    cout << "||\t\t\t\tTurnurile din Hanoi                                                ||\n" << endl;
+    cout << "||\tSe dau trei tije A, B, C si N discuri de diametre diferite, stivuite initial pe tija A     ||\n"
+        "||\tin ordinea descrescatoare a diametrelor, formand un con. Ss se determine secventa de       ||\n"
+        "||\tmutari ale discurilor de pe tija initială A catre tija finala C, folosind B ca tija ,      ||\n"
+        "||\tavand în vedere urmatoarele restrictii :                                                   ||\n"
+        "||\t  a. la fiecare miscare se va muta un singur disc                                           ||\n"
+        "||\t  b. un disc se poate plasa doar asupra unui disc mai mare ca diametru.                    ||" << endl;
+    cout << "| ==================================================================================================|" << endl;
+   system("pause");  system("cls");
 }
-//void meniu()
-//{
-//    enum menu{iesire, reg,joaca};
-//    do {
-//        switch (menu)
-//        {
-//        case iesire:
-//            exit(1);
-//            break;
-//        case reg:
-//            reguli();
-//            break;
-//        case joaca:
-//            int N;
-//            cout << "Cate discuri?\n n= ";
-//            cin >> N;
-//            turnurileHanoi(N, 'A', 'C', 'B');
-//            break;
-//        default:
-//            break;
-//        }
-//    } while (1);
-//}
+void meniu()
+{
+    int opt;
+    enum menu{iesire, reg,joaca}menu;
+    do {
+        cout << "1. Regulile jocului" << endl;
+        cout << "2. Joaca. Alegere numar de discuri." << endl;
+        cout << "0. Iesire..." << endl;
+       // cout << "3. " << endl;
+        cout << "Optiune: "; cin >> opt;
+        switch (opt)
+        {
+            
+        case iesire:
+            exit(1);
+            break;
+        case reg:
+            reguli();
+            break;
+        case joaca:
+            int N;
+            cout << "Cate discuri?\n n= ";
+            cin >> N;
+            turnurileHanoi(N, 'A', 'C', 'B');
+            break;
+        default:
+            break;
+        }
+    } while (1);
+}
 
 int main()
 {
     int N;
     
     reguli();
-    system("color D");
-    cout << "Cate discuri?\n n= ";
-    cin >> N;
-    turnurileHanoi(N, 'A', 'C', 'B');
+    //system("color D");
+   // meniu();
+   // cout << "Cate discuri?\n n= ";
+    //cin >> N;
+    //turnurileHanoi(N, 'A', 'C', 'B');
     return 0;
 }
